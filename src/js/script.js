1c36cbfe27726d0d5a87923e63c79657abee2a68
Yasuofack <aidinsparta09@gmail.com>
2022-05-19 10:22:20 +0600

    added file readme.md

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        // nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+
+    });
+});
